91c3eb0466a431d0c571ff8134642096fc88f4d7
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-01 18:41:51 -0500

    Public folder, npm, and server setup

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require(‘body-parser’);
+const PORT = 5000;
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+});
