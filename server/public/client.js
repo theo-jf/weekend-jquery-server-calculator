712d5ae651af9d8eb3da82f28b4edde551542b68
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-02 10:21:28 -0500

    Created client side function to type and POST functions to server, and display history with GET request

--- server/public/client.js
+++ server/public/client.js
@@ -2,7 +2,10 @@ $(readyNow);
 
 function readyNow() {
     // TODO:
+
     // Listener for #equals --> post request to server
+    $('#equals').on('click', '#display', submitEquation);
+
     // After (.then) post request, get request function to retrieve result
     // Put result back into input like a real calculator
     // Calculation gets appended on server side to array which is loop displayed in history table
@@ -13,10 +16,55 @@ function readyNow() {
 
     // Listeners for numbers (one giant listener on #calculator, (this) to get number?)
     // All client side(?) take pressed number / operator and put at the end of input. 
+    $('#calculator').on('click', '.button', appendDisplay);
 
     // listener on entire table for click on history to re-run calculation (this)
     // Just re-push old calculation and do original post request again
     // Put answer in input, calculation at the top of history
+    $('#historyTable').on('click', '.history', submitEquation);
 
     // Don't forget .toFixed(#)! Look up how many digits a normal calculator goes until
+}
+
+function submitEquation() {
+    let calculation = $(this).text();
+    $.ajax({
+        type: 'POST',
+        url: '/function',
+        data: {calculation}
+    }).then(function(response) {
+        console.log(response);
+        getAnswer();
+    });
+}
+
+function appendDisplay() {
+    let symbol = $(this).text();
+    $('#display').append(`${symbol}`);
+}
+
+function getAnswer() {
+    $.ajax({
+        type: 'GET',
+        url: '/function'
+    }).then(function(response) {
+        $('#display').append(response);
+        displayHistory();
+    })
+}
+
+function displayHistory() {
+    $.ajax({
+        type: 'GET',
+        url: '/history'
+    }).then(function(response) {
+        $('#historyTableBody').empty();
+        for (let calculation of response) {
+            $('#historyTableBody').append(`
+                <tr>
+                    <td class="history">${calculation}</td>
+                </tr>
+            `);
+        }
+    });
 }
